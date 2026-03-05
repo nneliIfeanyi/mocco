0318edd5cfe544d81f76a51ad7e6ba8fcad706c8
nneliifeanyi <stanvicbest@gmail.com>
2026-03-05 03:32:05 +0100

    live DB turn on

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,5 +1,11 @@
 <?php
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "mocco_db");
+
+// uncomment the line below to switch to a live database
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "mocco_db");
+define("DB_USER", "revivall_mocco-admin");
+define("DB_PASS", "Avalanche@25");
+define("DB_NAME", "revivall_mocco");
